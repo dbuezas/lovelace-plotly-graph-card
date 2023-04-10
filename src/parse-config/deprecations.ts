d777f6b6d4f5b6bc83f5099102a4ab4e9bc5aee6
David Buezas <david.buezas@gmail.com>
2023-04-10 21:02:11 +0200

    Add links to migration help on deprecation errors

--- src/parse-config/deprecations.ts
+++ src/parse-config/deprecations.ts
@@ -7,20 +7,20 @@ export default function getDeprecationError(path: string, value: any) {
 }
 function _getDeprecationError(path: string, value: any) {
   if (path.match(/^no_theme$/))
-    return "renamed to ha_theme (inverted logic) in v3.0.0";
+    return `renamed to <a href="https://github.com/dbuezas/lovelace-plotly-graph-card#home-assistant-theming">ha_theme</a> (inverted logic) in v3.0.0`;
   if (path.match(/^no_default_layout$/))
-    return "replaced with more general raw_plotly_config in v3.0.0";
+    return `replaced with more general <a href="https://github.com/dbuezas/lovelace-plotly-graph-card#raw-plotly-config">raw-plotly-config</a> in v3.0.0. See <a href="https://github.com/dbuezas/lovelace-plotly-graph-card#no_default_layout">layout migration guide</a>.`;
   if (path.match(/^offset$/)) return "renamed to time_offset in v3.0.0";
   if (path.match(/^entities\.\d+\.offset$/)) {
     try {
       parseTimeDuration(value);
-      return 'renamed to time_offset in v3.0.0 to avoid conflicts with <a href="https://plotly.com/javascript/reference/bar/#bar-offset">bar-offsets</a>';
+      return 'renamed to time_offset in v3.0.0 to avoid conflicts with <a href="https://plotly.com/javascript/reference/bar/#bar-offset">bar-offsets</a>.';
     } catch (e) {
       // bar-offsets are numbers without time unit
     }
   }
   if (path.match(/^entities\.\d+\.lambda$/))
-    return "removed in v3.0.0, use filters instead";
+    return `removed in v3.0.0, use <a href="https://github.com/dbuezas/lovelace-plotly-graph-card#filters">filters</a> instead. See <a href="https://github.com/dbuezas/lovelace-plotly-graph-card#lambda">lambda migration guide</a>.`;
   if (path.match(/^entities\.\d+\.show_value\.right_margin$/))
     return "removed in v3.0.0, use `true` and set the global `time_offset` or `layout.margins.r` to make space at the right. ";
   if (path.match(/^significant_changes_only$/))
