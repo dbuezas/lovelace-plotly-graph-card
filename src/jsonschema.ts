344756f25a027be2718795144e60bc04452b00b5
David Buezas <david.buezas@gmail.com>
2024-10-13 15:27:54 +0200

    Schema fixes

--- src/jsonschema.ts
+++ src/jsonschema.ts
@@ -1,14 +1,16 @@
 import { InputConfig } from "./types";
 
+type Whitespace = " " | "\t" | "\n" | "\r";
+
 type With$fn<T> = {
   [K in keyof T]:
     | (T[K] extends (infer U)[]
         ? With$fn<U>[] // handle arrays recursively
         : T[K] extends object
-        ? With$fn<T[K]> // handle objects recursively
-        : T[K]) // retain original type
-    | (string & `$ex ${string}`)
-    | (string & `$fn ${string}`); // allow string starting with $ex or $fn
+          ? With$fn<T[K]> // handle objects recursively
+          : T[K]) // retain original type
+    | (string & `${Whitespace | ""}$ex${Whitespace}${string}`)
+    | (string & `${Whitespace | ""}$fn${Whitespace}${string}`); // allow string starting with $ex or $fn with optional whitespace
 };
 
-export type JsonSchemaRoot = With$fn<InputConfig>
+export type JsonSchemaRoot = With$fn<InputConfig>;
